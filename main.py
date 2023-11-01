4ce5a2ba38ee02b6a8562cb7a34b4905ddb581e1
krizhnaa <krizhnaprem@gmail.com>
2023-11-01 20:48:41 +0530

    MAIN PROJECT COMPLETED

--- main.py
+++ main.py
@@ -1,33 +1,43 @@
 import art
 import game_data
 import random
+import os
 
 print(art.logo)
 
 game_over = False
 user_point = 0
+show_point = False
 
 opt = ['A','B']
 
-opa = random.randint(0,len(game_data.data)-1)
-opb = random.randint(0,len(game_data.data)-1)
+while game_over != True:
+    opa = random.randint(0,len(game_data.data)-1)
+    opb = random.randint(0,len(game_data.data)-1)
 
-print(f"Compare {opt[0]} : {game_data.data[opa]['name']}, a {game_data.data[opa]['description']}, from {game_data.data[opa]['country']}")
-print(game_data.data[opa]['follower_count'])
+    if show_point == True:
+        print(f"Your points are : {user_point}")
 
-print(art.vs)
+    print(f"Compare {opt[0]} : {game_data.data[opa]['name']}, a {game_data.data[opa]['description']}, from {game_data.data[opa]['country']}")
+    print(game_data.data[opa]['follower_count'])
 
-print(f"Compare {opt[1]} : {game_data.data[opb]['name']}, a {game_data.data[opb]['description']}, from {game_data.data[opb]['country']}")
-print(game_data.data[opb]['follower_count'])
-user_inp = input("Who has the most followers, Option 'A' or 'B' : ")
+    print(art.vs)
 
-if user_inp == opt[0] and game_data.data[opa]['follower_count'] > game_data.data[opb]['follower_count']:
-    print("You are awarded a point")
-    user_point+=1
-elif user_inp == opt[1] and game_data.data[opb]['follower_count'] > game_data.data[opa]['follower_count']:
-    print("You are awarded a point")
-    user_point+=1
-else:
-    print("You Lose")
-    print(f"Your total points are : {user_point}")
-    game_over = True
+    print(f"Compare {opt[1]} : {game_data.data[opb]['name']}, a {game_data.data[opb]['description']}, from {game_data.data[opb]['country']}")
+    print(game_data.data[opb]['follower_count'])
+    user_inp = input("Who has the most followers, Option 'A' or 'B' : ")
+
+    if user_inp == opt[0] and game_data.data[opa]['follower_count'] > game_data.data[opb]['follower_count']:
+        print("You are awarded a point")
+        user_point+=1
+        os.system('cls')
+        show_point = True
+    elif user_inp == opt[1] and game_data.data[opb]['follower_count'] > game_data.data[opa]['follower_count']:
+        print("You are awarded a point")
+        user_point+=1
+        os.system('cls')
+        show_point = True
+    else:
+        print("You Lose")
+        print(f"Your total points are : {user_point}")
+        game_over = True

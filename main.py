b99213e51fc7fbe1aeb82652b1faee98f543228a
krizhnaa <krizhnaprem@gmail.com>
2023-11-01 21:01:02 +0530

    Final Commit

--- main.py
+++ main.py
@@ -22,12 +22,12 @@ while game_over != True:
         print(f"Your points are : {user_point}")
 
     print(f"Compare {opt[0]} : {game_data.data[opa]['name']}, a {game_data.data[opa]['description']}, from {game_data.data[opa]['country']}")
-    print(game_data.data[opa]['follower_count'])
+    # print(game_data.data[opa]['follower_count'])
 
     print(art.vs)
 
     print(f"Compare {opt[1]} : {game_data.data[opb]['name']}, a {game_data.data[opb]['description']}, from {game_data.data[opb]['country']}")
-    print(game_data.data[opb]['follower_count'])
+    # print(game_data.data[opb]['follower_count'])
     user_inp = input("Who has the most followers, Option 'A' or 'B' : ").upper()
 
     if user_inp == opt[0] and game_data.data[opa]['follower_count'] > game_data.data[opb]['follower_count']:
